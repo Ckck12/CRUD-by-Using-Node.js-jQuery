29415b24d9c9b7fffa276c1501102ff90fa4ac43
chan <100400mjkim@gmail.com>
2022-07-21 20:49:54 +0900

    login function

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const postsRouter = require("./routes/posts")
-const cors = require('cors')
-const bodyParser = require("body-parser")
+const express = require("express");
+const mongoose = require("mongoose");
+const postsRouter = require("./routes/posts");
+const userRouter = require("./routes/user");
 
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
-
 
 //DB 연결
 mongoose.connect("mongodb://localhost:27017/myapp");
 
 mongoose.connection.on("connected", () => {
-    console.log('DB connect success');
-})
+  console.log("DB connect success");
+});
 
 mongoose.connection.on("error", (err) => {
-    console.log(err);
+  console.log(err);
 });
 
 app.use(cors());
@@ -24,7 +24,8 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/posts", postsRouter);
 
+app.use("/user", userRouter);
 
 app.listen(8080, () => {
-    console.log('server open');
-})
+  console.log("server open");
+});
